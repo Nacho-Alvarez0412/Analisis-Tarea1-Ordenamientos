f28d78b273f0aadb0dcb2fea87152366c02133ac
Ignacio Alvarez <52050956+Nacho-Alvarez0412@users.noreply.github.com>
2020-02-16 14:36:42 -0600

    Creation of the C# Files
    Archivos de C# con algoritmos para crear las listas y ordenarlas.
    
    Ordenamientos usados :
    BubbleSort
    QuickSort

---
+++ Ordenamientos Tarea1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using Ordenamientos_Tarea1.Properties;
+
+namespace Ordenamientos_Tarea1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] list1 = ListManager.listGenerator(4);
+            Console.WriteLine(ListManager.ToString(list1));
+            ListManager.bubbleSort(list1);
+            Console.WriteLine(ListManager.ToString(list1));
+        }
+    }
+}
---
+++ Ordenamientos Tarea1/Properties/ListManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+
+namespace Ordenamientos_Tarea1.Properties
+{
+    public static class ListManager
+    {
+        public static int[] listGenerator(int size)
+        {
+            int[] list = new int[size];
+            Random random = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                list[i] = random.Next(0, 100000);
+            }
+            return list;
+        }
+
+        public static string ToString(int[] list)
+        {
+            string printedList = "";
+
+            foreach (int element in list)
+            {
+                printedList = printedList + element + ", ";
+            }
+            return printedList;
+        }
+        
+        public static void quickSort(int[] vector, int primero, int ultimo)
+        {
+            int i, j, central;
+            double pivote;
+            central = (primero + ultimo) / 2;
+            pivote = vector[central];
+            i = primero;
+            j = ultimo;
+            do
+            {
+                while (vector[i] < pivote) i++;
+                while (vector[j] > pivote) j--;
+                if (i <= j)
+                {
+                    int temp;
+                    temp = vector[i];
+                    vector[i] = vector[j];
+                    vector[j] = temp;
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+
+            if (primero < j)
+            {
+                quickSort(vector, primero, j);
+            }
+            if (i < ultimo)
+            {
+                quickSort(vector, i, ultimo);
+            }
+        }
+        
+        public static void bubbleSort(int[] array)
+        {
+            int length = array.Length;
+
+            int temp = array[0];
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i+1; j < length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+
+                        array[i] = array[j];
+
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+    
+    
+}
