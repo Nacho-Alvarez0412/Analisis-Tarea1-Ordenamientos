8163e731d6a72ece85bf3be3c151d86eed13ecda
SebasG <gamboasebas@gmail.com>
2020-02-18 22:51:34 -0600

    Ordenamientos en Proyecto Unity
    Listo el proyecto de unity con los algoritmos, ahora si falta graficar

--- Ordenamientos Tarea1/Program.cs
+++ Ordenamientos Tarea1/Program.cs
@@ -15,19 +15,19 @@ namespace Ordenamientos_Tarea1
                 long execTime = ListManager.bubbleSort(list1);
                 Console.WriteLine("With an array of "+arrayLength+" numbers, the execution time for bubble sort was: "+execTime+ " miliseconds");
             }
-            
+
             for (int i = 1; i < 101; i++)
             {
-                int arrayLength = i*10000;
+                int arrayLength = i * 15000;
                 int[] list2 = ListManager.listGenerator(arrayLength);
                 var watch = new System.Diagnostics.Stopwatch();
                 watch.Start();
-                ListManager.quickSort(list2,0,list2.Length-1);
+                ListManager.quickSort(list2, 0, list2.Length - 1);
                 watch.Stop();
                 long execTime = watch.ElapsedMilliseconds;
-                Console.WriteLine("With an array of "+arrayLength+" numbers, the execution time for quicksort was: "+execTime+" miliseconds");
+                Console.WriteLine("With an array of " + arrayLength +
+                                  " numbers, the execution time for quicksort was: " + execTime + " miliseconds");
             }
-            
         }
     }
 }
---
+++ Ordenamientos/Assets/SortingScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i < 101; i++)
+        {
+            int arrayLength = i*100;
+            int[] list1 = ListManager.listGenerator(arrayLength);
+            long execTime = ListManager.bubbleSort(list1);
+            print("With an array of "+arrayLength+" numbers, the execution time for bubble sort was: "+execTime+ " miliseconds");
+        }
+
+        for (int i = 1; i < 101; i++)
+        {
+            int arrayLength = i * 1500;
+            int[] list2 = ListManager.listGenerator(arrayLength);
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            ListManager.quickSort(list2, 0, list2.Length - 1);
+            watch.Stop();
+            long execTime = watch.ElapsedMilliseconds;
+            print("With an array of " + arrayLength +
+                              " numbers, the execution time for quicksort was: " + execTime + " miliseconds");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
